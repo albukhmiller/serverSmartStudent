6b64cbe1a87244f2e1b8e7603f0838ee0370e3c4
Alex Bukhmiller <alex@tech2.cpd>
2018-01-22 15:39:41 -0800

    connect db

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var url = 'mongodb://alexbukhmiller:ulupos5555@ds133127.mlab.com:33127/smart_student';
+var url = 'mongodb://heroku_g2dnzfps:ulupos5555@ds111608.mlab.com:11608/heroku_g2dnzfps';
 mongoose.Promise = global.Promise;
 mongoose.connect(url, { useMongoClient: true })
     .then(() => console.log("Connection is success!"))
