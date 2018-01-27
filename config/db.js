de0642306c83963bd36c002df5080dac85b0d1b0
Alex Bukhmiller <alex@MacBook-Air-Alex.local>
2018-01-27 00:19:02 -0800

    final commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var url = 'url mlab database';
+var url = 'mongodb://alexbukhmiller:ulupos5555@ds133127.mlab.com:33127/smart_student';
 mongoose.Promise = global.Promise;
 mongoose.connect(url, { useMongoClient: true })
     .then(() => console.log("Connection is success!"))
